6133c885921e0decc0a3f85c07435c96d681fbcc
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-11-23 23:10:07 +0100

    Fix vagrant setup, move vendor to /opt

--- app/vagrant/autoload.php
+++ app/vagrant/autoload.php
@@ -5,7 +5,7 @@ use Composer\Autoload\ClassLoader;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
-$vendorDir = getenv('COMPOSER_VENDOR_DIR') ?: '/tmp/symfony/vendor';
+$vendorDir = getenv('COMPOSER_VENDOR_DIR') ?: '/opt/symfony/vendor';
 
 /**
  * @var ClassLoader $loader
