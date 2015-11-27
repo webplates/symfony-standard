eb26eb996d18677e72fdaaf707f3cf26c1aab386
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-11-27 11:43:41 +0100

    Add Node+Bower+Gulp

---
+++ src/AppBundle/Resources/assets/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$ = window.$ = window.jQuery = require('jquery');
+var domready = require('domready');
+require('bootstrap');
+
+var App = {
+    init: function() {
+
+    }
+};
+
+domready(function() {
+    App.init();
+});
