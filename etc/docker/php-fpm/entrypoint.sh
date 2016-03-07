8df0c64e09f2f011df890d5aec796c795d2b7fe8
Márk Sági-Kazár <mark.sagi-kazar@db-n.com>
2016-03-07 10:25:10 +0100

    Only update the uid as gid might already be reserved

--- etc/docker/php-fpm/entrypoint.sh
+++ etc/docker/php-fpm/entrypoint.sh
@@ -3,11 +3,9 @@
 set -e
 
 MYUID=`stat -c "%u" .`
-MYGID=`stat -c "%g" .`
 
 if [ "$MYUID" -gt '0' ]; then
     usermod -u $MYUID www-data
-    groupmod -g $MYGID www-data
 fi
 
 if [[ "$1" = 'php-fpm' || "$1" = '' ]]; then
