52cc44f8472202ccd99d9cbbb74520282cdedf6a
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-03-17 22:22:39 +0100

    Improve dev deployment stage

--- etc/deploy/stages/dev.rb
+++ etc/deploy/stages/dev.rb
@@ -1,1 +1,11 @@
 server "dev.symfony.com", user: "fabien", roles: [:app, :web]
+
+set :composer_install_flags, "--no-interaction --quiet --optimize-autoloader"
+
+set :symfony_env, "dev"
+
+after "deploy:updated", :build do
+    invoke "doctrine:migrations:migrate"
+    invoke "symfony:console", "h:d:f:l", "--no-interaction"
+    invoke "symfony:console", "cache:clear", "--env", "prod"
+end
