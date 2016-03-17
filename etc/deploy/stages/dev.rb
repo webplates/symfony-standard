5e0278999f23914ff07765d4bf39d5ae9a081bbe
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-03-17 22:37:00 +0100

    Fix number of arguments

--- etc/deploy/stages/dev.rb
+++ etc/deploy/stages/dev.rb
@@ -7,5 +7,5 @@ set :symfony_env, "dev"
 after "deploy:updated", :build do
     invoke "doctrine:migrations:migrate"
     invoke "symfony:console", "h:d:f:l", "--no-interaction"
-    invoke "symfony:console", "cache:clear", "--env", "prod"
+    invoke "symfony:console", "cache:clear", "--env prod"
 end
