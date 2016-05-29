2caed9be315d5cf0ca12c708dfb273b4f60f2cf0
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-05-30 01:09:51 +0200

    Remove kernel.storage_dir parameter, relocate cache and log directories in vagrant

--- app/autoload.php
+++ app/autoload.php
@@ -5,25 +5,10 @@ use Composer\Autoload\ClassLoader;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
-// Vendor and var can be stored outside of the base path
-// This will be available as an option called "kernel.storage_dir"
-$storageDir = getenv('SYMFONY__kernel__storage_dir');
-
-if (empty($storageDir)) {
-    $storageDir = dirname(__DIR__);
-
-    putenv('SYMFONY__kernel__storage_dir='.$storageDir);
-    $_SERVER['SYMFONY__kernel__storage_dir'] = $storageDir;
-    $_ENV['SYMFONY__kernel__storage_dir'] = $storageDir;
-}
-
-// Don't rely on this constant
-define('KERNEL_STORAGE_DIR', $storageDir);
-
 /**
  * @var ClassLoader $loader
  */
-$loader = require KERNEL_STORAGE_DIR.'/vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
