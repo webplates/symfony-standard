04ad9b651bc2323f0be909b9aa19bbcbc4cd3cc5
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-08-10 21:03:21 +0200

    Remove gosu from frontend dev container

--- etc/docker/dev/frontend/entrypoint.sh
+++ etc/docker/dev/frontend/entrypoint.sh
@@ -9,13 +9,4 @@ if [[ "$MYUID" -gt '0' && "$MYUID" != `id -u ${CUSER}` ]]; then
     usermod -u ${MYUID} ${CUSER}
 fi
 
-case "$1" in
-    "root")
-      exec /bin/bash ;;
-    "shell")
-      gosu ${CUSER} "/bin/bash" ;;
-    "")
-      gosu ${CUSER} "/bin/bash" ;;
-    *)
-      gosu ${CUSER} "$@" ;;
-esac
+exec "$@"
