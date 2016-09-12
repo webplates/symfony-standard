fcd3ff4e89b8e56a72a9d5627e1ba5e6af17671e
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-09-12 13:34:16 +0200

    Update Symfony

--- app/autoload.php
+++ app/autoload.php
@@ -3,13 +3,9 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-error_reporting(error_reporting() & ~E_USER_DEPRECATED);
-
-/**
- * @var ClassLoader $loader
- */
+/** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
