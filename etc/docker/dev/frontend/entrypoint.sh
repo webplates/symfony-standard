55ca06c6de867e477dc98a165c9b18177ff711d3
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-10-19 13:06:27 +0200

    Add more frontend build commands

--- etc/docker/dev/frontend/entrypoint.sh
+++ etc/docker/dev/frontend/entrypoint.sh
@@ -9,4 +9,11 @@ if [[ "$MYUID" -gt '0' && "$MYUID" != `id -u ${CUSER}` ]]; then
     usermod -u ${MYUID} ${CUSER}
 fi
 
-exec "$@"
+case "$1" in
+    "build")
+        exec gulp build ;;
+    "watch")
+        exec gulp watch ;;
+    *)
+        exec "$@" ;;
+esac
